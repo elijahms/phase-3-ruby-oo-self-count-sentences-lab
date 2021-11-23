346c18c2e333268a21ec24f1f1b9a4c34a71ff93
Elijah Silverman <elijahmsilverman@gmail.com>
2021-11-23 12:16:55 -0500

    commit tree d226303a4bfc67510574aecdd70f8d2f89174410

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    string_arr = self.split((/\.|\?|!/))
+
+    string_arr.filter{|sentence| !sentence.empty? binding.pry}.length
+    
+    # string_arr.length
+    
+    # self.split("").map { |char| 
+    # if (char == "." || char == "?" || char == "!")
+    #   count += 1
+    # end 
+    # }
 
   end
 end
